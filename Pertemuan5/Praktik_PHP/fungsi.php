9335be2ca910491eeecd596275193f2cf999e883
ISHALL <75568324+faishal-ai@users.noreply.github.com>
2024-10-02 01:27:18 +0700

    Add files via upload

---
+++ Pertemuan5/Praktik_PHP/fungsi.php
@@ -0,0 +1,41 @@
+<?php
+//praktikum1
+// function perkenalan() {
+//     echo "Assalamualikum";
+//     echo"Perkenalkan, nama saya Faishal</br>";
+//     echo "Senang berkenalan dengan Anda</br>";
+// }
+
+// perkenalan();
+
+//praktikum2
+// function perkenalan($nama, $salam="Assalamualikum") {
+//     echo $salam. ",";
+//     echo"Perkenalkan, nama saya ".$nama."</br>";
+//     echo "Senang berkenalan dengan anda</br>";
+// }
+
+// perkenalan("Faishal","Hallo");
+
+// echo "<hr>";
+
+// $saya = "Harist";
+// $ucapanSalam = "Selamat pagi";
+
+// perkenalan($saya,$ucapanSalam);
+
+function perkenalan($nama, $salam="Assalamualikum") {
+    echo $salam. ",";
+    echo"Perkenalkan, nama saya ".$nama."</br>";
+    echo "Senang berkenalan dengan anda</br>";
+}
+
+perkenalan("Faishal","Hallo");
+
+echo "<hr>";
+
+$saya = "Harist";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya);
+?>
